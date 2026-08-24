9e444fd2a6cc1e32bbd65a39cbca1e44443309f8
Sarveshwar Hirve <sarveshwar@gmail.com>
2026-08-24 16:25:01 +0530

    Added Songs

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
 console.log('lets write js');
+
+async function getSongs() {
+    let a = await fetch("http://127.0.0.1:3000/songs/")
+    let response = await a.text();
+   
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if(element.href.endsWith(".mp3")){
+            songs.push(element.href)
+        }
+        
+    }
+
+    return songs
+    
+    
+}
+
+async function main(){
+    // get the list of all songs
+    let songs = await getSongs()
+    console.log(songs);
+
+  let songUL =  document.querySelector(".songlist").getElementsByTagName("ul")[0]
+  for (const song of songs) {
+    songUL.innerHTML = songUL.innerHTML + `<li>${song}</li>`;
+    
+  }
+
+    //play the first song
+var audio = new Audio(songs[0])
+audio.play();
+
+audio.addEventListener("loadeddata", ()=>{
+    console.log(audio.duration, audio.currentSrc, audio.currentTime);
+    
+});
+
+    
+} 
+
+main()
